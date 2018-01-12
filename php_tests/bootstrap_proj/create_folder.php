63b7972730544fe7f50218112be82e30c806e3c0
l.vallet <luc.vallet@cpam-loireatlantique.cnamts.fr>
2018-01-12 16:17:36 +0100

    doc..;

--- php_tests/bootstrap_proj/create_folder.php
+++ php_tests/bootstrap_proj/create_folder.php
@@ -1,5 +1,37 @@
 <?php
 
 echo "variables ";
+//var_dump($_POST)
+
+//C:\\Users\\VALLET-03846\\Documents\\temp\\
+
+
+foreach ($_POST as $key => $value) {
+    //do something
+    echo $key . ' has the value of ' . $value."<br>";
+}
+// $path="C:\\Users\\VALLET-03846\\Documents\\temp\\".$_POST["nom"];
+// echo $path;
+//
+// if (!file_exists($path)) {
+//   echo "<br>le repertoire ".$_POST['nom']." est pas là je le crée";
+//   mkdir("C:\\Users\\VALLET-03846\\Documents\\temp\\".$_POST['nom'], 0777, true);
+// }
+// else {
+//   echo "repertoire existant !";
+// }
+
+$path2="W:\\DRHM\\PINF\\SDI\\Commun\\Luc\\".$_POST["nom"];
+ if (!file_exists($path2)) {
+   echo "<br>le repertoire ".$_POST['nom']." est pas là je le crée";
+   mkdir("W:\\DRHM\\PINF\\SDI\\Commun\\Luc\\".$_POST['nom'], 0777, true);
+ }
+ else {
+   echo "repertoire existant !";
+ }
+
+/*if (!file_exists('C:\Users\VALLET-03846\Documents\temp\'.$_POST['nom'])) {
+  mkdir('C:\Users\VALLET-03846\Documents\temp\'.$_POST['nom'], 0777, true);
+}*/
 
  ?>
