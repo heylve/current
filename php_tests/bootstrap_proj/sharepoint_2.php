50a29e329feac79781b34161a924053309882a09
l.vallet <luc.vallet@cpam-loireatlantique.cnamts.fr>
2018-01-18 16:47:19 +0100

    summary appli med doc

--- php_tests/bootstrap_proj/sharepoint_2.php
+++ php_tests/bootstrap_proj/sharepoint_2.php
@@ -2,7 +2,7 @@
 require __DIR__ . '/vendor/autoload.php';
 use Thybag\SharePointAPI;
 //$sp = new SharePointAPI('LUC.VALLET@cpam-loireatlantique.cnamts.fr', '', 'http://w11440101shp/sites/Projets-SDMA/_vti_bin/DspSts.asmx?WSDL');
-$sp = new SharePointAPI('VALLET-03846', 'BepiColombo', './sp_wsdl.wsdl','NTML');
+$sp = new SharePointAPI('LUC.VALLET@cpam-loireatlantique.cnamts.fr', '', './sp_wsdl.wsdl','NTML');
 //$sp->getLists();
 //$sp->query('/Projets-SDMA')->limit(10)->get();
 $sp->getLists();
