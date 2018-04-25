ca5465ac43d7d3699861a02b9f71987ed7d9df59
l.vallet <luc.vallet@cpam-loireatlantique.cnamts.fr>
2018-04-25 14:36:23 +0200

    slim..cot

---
+++ php_tests/slim_cot/project/src/public/index.php
@@ -0,0 +1,52 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../../../vendor/autoload.php';
+
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = 'localhost';
+$config['db']['user']   = 'root';
+$config['db']['pass']   = '';
+$config['db']['dbname'] = 'cotations';
+
+
+
+
+$app = new \Slim\App(["settings" => $config]);
+
+$container = $app->getContainer();
+
+$container['db'] = function ($config) {
+	$db = $config['db'];
+	$pdo = new PDO('mysql:host=' . $db['host'] . ';dbname=' . $db['dbname'],
+		$db['user'], $db['pass']);
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+	return $pdo;
+};
+
+$container['logger'] = function($c) {
+	$logger = new \Monolog\Logger('my_logger');
+	$file_handler = new \Monolog\Handler\StreamHandler('../logs/app.log');
+	$logger->pushHandler($file_handler);
+	return $logger;
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+	$name = $args['name'];
+	$response->getBody()->write("Hello, $name");
+	$this->logger->addInfo('Something interesting happened');
+	$this->db->query("select  * from ccam;");
+	//$mapper = new CotationMapper($this->db);
+	return $response;
+});
+
+
+
+$app->run();
+
+?>
